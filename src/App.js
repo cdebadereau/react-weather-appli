2fe540ed84919e02516e2060727be52832690626
cdebadereau <98481801+cdebadereau@users.noreply.github.com>
2025-01-28 17:49:03 +0100

    Added the GitHub repository url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      <h1>Weather Application</h1>
+      <small>
+        This project was coded by Cyrielle de Badereau de Saint Martin and is{" "}
+        <a
+          href="https://github.com/cdebadereau/react-weather-appli"
+          target="_blank"
+        >
+          open-sourced on Github.
+        </a>
+      </small>
+    </div>
+  );
 }
