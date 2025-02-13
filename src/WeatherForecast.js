85e264e52abef8a014394c30a3ccff975ad1333c
cdebadereau <98481801+cdebadereau@users.noreply.github.com>
2025-02-13 12:13:39 +0100

    Created a WeatherForecastDay to take care of the forecast data

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,37 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import WeatherIcon from "./WeatherIcon";
+import WeatherForecastDay from "./WeatherForecastDay";
 
 import "./WeatherForecast.css";
 import axios from "axios";
 
 export default function WeatherForecast(props) {
+  const [loaded, setLoaded] = useState(false);
+  const [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    console.log(response.data.daily);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  console.log(props);
-
-  let apiKey = "96668c3bo8171924ab023b1atd05c04f";
-  let city = props.city;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
-
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="WeatherForecast">
-      <div className="row">
-        <div className="col">
-          <div className="forecast-day-container">
-            <div className="forecast-day">Tue</div>
-
-            <WeatherIcon code="rain-day" size={30} />
-            <div className="forecast-temperatures mt-3">
-              <span className="forecast-temperature-max">19°</span>{" "}
-              <span className="forecast-temperature-min">10°</span>
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          <div className="col">
+            <div className="forecast-day-container">
+              <WeatherForecastDay data={forecast[0]} />
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "96668c3bo8171924ab023b1atd05c04f";
+    let city = props.city;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
 }
+
+//1. Design the forecast layout
+//2. Make the API call
+//3. Create a new state when it is loading (false by default) and move the API call inside an if statement
+//4. Create a new state to store the data
+//5. Update the UI with the data (temperatures, time, icon)
+//6.Create a new component that includes the forecast day (a lot easier to maintain)
---
+++ src/WeatherForecastDay.js
@@ -0,0 +1,37 @@
+import React from "react";
+import WeatherIcon from "./WeatherIcon";
+
+export default function WeatherForecastDay(props) {
+  function maxTemperature() {
+    let maxTemperature = Math.round(props.data.temperature.maximum);
+    return `${maxTemperature}°`;
+  }
+
+  function minTemperature() {
+    let minTemperature = Math.round(props.data.temperature.minimum);
+    return `${minTemperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    let day = date.getDay();
+    return days[day];
+  }
+
+  return (
+    <div className="WeatherForecastDay">
+      <div className="forecast-day">{day()}</div>
+      <WeatherIcon code={props.data.condition.icon} size={30} />
+      <div className="forecast-temperatures mt-3">
+        <span className="forecast-temperature-max">{maxTemperature()}</span>{" "}
+        <span className="forecast-temperature-min">{minTemperature()}</span>
+      </div>
+    </div>
+  );
+}
+
+//1. Import the UI in this new component
+//2. The response of the API call is no longer in the forecast state, but in the props.data (data is the props given to the component in WeatherForecast.js) DON'T FORGET TO IMPORT ALSO THE WEATHERICON COMPONENT IN THIS NEW COMPONENT. Change every data with props.data
+//3. Create a new function for each temperature to rounded it and call the fonction inside the UI
+//4. Create a new function to take care of the date and indicate the days
