b22bdc79640cfc2fab6fbc66cf089e24e94f164b
cdebadereau <98481801+cdebadereau@users.noreply.github.com>
2025-03-06 21:34:41 +0100

    Added link to portfolio

--- src/App.js
+++ src/App.js
@@ -8,8 +8,15 @@ export default function App() {
       <div className="container">
         <Weather defaultCity="Paris" />
         <small>
-          This project was coded by Cyrielle de Badereau de Saint Martin and is
-          open-sourced on{" "}
+          This project was coded by{" "}
+          <a
+            href="https://cyrielle-debadereau.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Cyrielle de Badereau de Saint Martin
+          </a>{" "}
+          and is open-sourced on{" "}
           <a
             href="https://github.com/cdebadereau/react-weather-appli"
             target="_blank"
